eeba03ecc7ddb66121a31cd58355f6c92445a524
noor <noortshepo@gmail.com>
2021-07-21 04:43:08 +0200

    added meta tags helmet

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <meta name="twitter:title" content="Footy ZA" />
         <meta name="twitter:description" content="Football blog by Mosia Noor-Tshepo" />
         <meta name="twitter:image"
-          content="https://images.unsplash.com/photo-1422728280635-45167d8b7197?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2748&q=80" />
+          content="https://cdn.sanity.io/images/3rw4tk2q/production/58c7afd8e55802c8b34d7ba0878908c29c5608cd-1350x759.jpg?rect=0,0,1349,759&w=1200&h=675&fit=crop&auto=format" />
 
 
       </Helmet>
